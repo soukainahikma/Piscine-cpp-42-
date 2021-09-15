4ca582e8ffa288d27242666d544bd669315c5bac
soukainahikma <soukainahikma@gmail.com>
2021-09-15 11:28:52 +0100

    cpp06 ex07 in progress

--- CppModule06/ex02/main.cpp
+++ CppModule06/ex02/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-
+#include <unistd.h>
 class Base
 {
-	/* virtual */ ~Base();
+	private:
+	public:
+		virtual  ~Base(){}
 };
 class A:public Base {};
 class B:public Base {};
@@ -12,7 +14,6 @@ Base * generate(void)
 {
 	srand(time(NULL));
 	int i = rand() % 3;
-	std::cout << i << std::endl;
 	switch (i)
 	{
 		case 0:
@@ -21,32 +22,45 @@ Base * generate(void)
 			return new B;
 		case 2:
 			return new C;
+		default:
+		{
+			std::cout << "non of the above" << std::endl;
+			return NULL;
+		}
 	}
-	return new Base;
 }
-// void identify(Base* p)
-// {
-	// if (p != NULL)
-	// {
-		// try{
-			// A *a = dynamic_cast<A *>(p);
-			// (void) a;
-		// }
-		// catch(std::bad_cast &bc)
-		// {
-			// std::cout <<bc.what() << std::endl;
-		// }
-	// }
-// }
-// void identify(Base& p)
-// {
-
-// }
+void identify(Base* p)
+{
+	
+	if(dynamic_cast<A*>(p))
+		std::cout << "A"<< std::endl;
+	else if(dynamic_cast<B*>(p))
+		std::cout << "B"<< std::endl;
+	else if(dynamic_cast<C*>(p))
+		std::cout << "C"<< std::endl;
+	else
+		std::cout << "Allocation failed" << std::endl;
+}
+void identify(Base& p)
+{
+	if(dynamic_cast<A*>(&p))
+		std::cout << "A"<< std::endl;
+	else if(dynamic_cast<B*>(&p))
+		std::cout << "B"<< std::endl;
+	else if(dynamic_cast<C*>(&p))
+		std::cout << "C"<< std::endl;
+	else
+		std::cout << "Allocation failed" << std::endl;
+}
 
 int main()
 {
 	Base *base;
 	base = generate();
+	identify(base);
+	sleep(1);
+	base = generate();
+	identify(base);
 	
 	return(0);
 
---
+++ CppModule07/ex01/iter.hpp
@@ -0,0 +1,10 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+#include <iostream>
+
+template <typename F>
+void iter(char *address,size_t size_,F f)
+{
+	
+}
+#endif
